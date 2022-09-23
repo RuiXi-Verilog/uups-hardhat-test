2ebff771f09b8cb63e69a31e3424ae5b01303c7c
RuiXi-Verilog <rui.x@verilog.solutions>
2022-09-23 12:12:05 -0700

    Init Commit

---
+++ test/MyToken.test.js
@@ -0,0 +1,26 @@
+// test/MyToken.test.js
+
+const { expect } = require('chai');
+const { ethers, upgrades } = require('hardhat');
+
+let myTokenV1;
+let myTokenV2;
+
+describe('MyToken', function () {
+  it('deploys', async function () {
+    const MyTokenV1 = await ethers.getContractFactory('MyTokenV1');
+    // await MyTokenV1.deploy();
+    // await upgrades.deployProxy(MyTokenV1);
+    myTokenV1 = await upgrades.deployProxy(MyTokenV1, { kind: 'uups' });
+  });
+
+  it('upgrades', async function () {
+    const MyTokenV2 = await ethers.getContractFactory('MyTokenV2');
+    myTokenV2 = await upgrades.upgradeProxy(myTokenV1.address, MyTokenV2);
+  });
+
+  it('addresses', async function () {
+    expect(myTokenV2.address).to.equal(myTokenV1.address);
+  });
+
+});
